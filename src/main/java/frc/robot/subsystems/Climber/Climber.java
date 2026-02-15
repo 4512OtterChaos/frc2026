c45643341620c138824e646271d3dbbe5ade4e43
4512 Otter Chaos <otterchaos4512@gmail.com>
2026-02-14 16:31:21 -0800

    flywheel sim -> elevator sim

--- src/main/java/frc/robot/subsystems/Climber/Climber.java
+++ src/main/java/frc/robot/subsystems/Climber/Climber.java
@@ -1,162 +1,201 @@
-package frc.robot.subsystems.Climber;
+    package frc.robot.subsystems.Climber;
 
-import com.ctre.phoenix6.BaseStatusSignal;
-import com.ctre.phoenix6.StatusSignal;
-import com.ctre.phoenix6.controls.MotionMagicVoltage;
+    import com.ctre.phoenix6.BaseStatusSignal;
+    import com.ctre.phoenix6.StatusSignal;
+    import com.ctre.phoenix6.controls.MotionMagicVoltage;
+    import com.ctre.phoenix6.controls.PositionVoltage;
+    import com.ctre.phoenix6.hardware.TalonFX;
+    import com.ctre.phoenix6.sim.ChassisReference;
+    import com.ctre.phoenix6.sim.TalonFXSimState;
 
-import com.ctre.phoenix6.hardware.TalonFX;
+    import edu.wpi.first.math.MathUtil;
+    import edu.wpi.first.math.system.plant.DCMotor;
+    import edu.wpi.first.math.system.plant.LinearSystemId;
+    import edu.wpi.first.units.AngleUnit;
+    import edu.wpi.first.units.measure.Angle;
+    import edu.wpi.first.units.measure.AngularVelocity;
+    import edu.wpi.first.units.measure.Current;
+    import edu.wpi.first.units.measure.Distance;
+    import edu.wpi.first.units.measure.LinearVelocity;
+    import edu.wpi.first.units.measure.Voltage;
+    import edu.wpi.first.wpilibj.RobotController;
+import edu.wpi.first.wpilibj.simulation.ElevatorSim;
+import edu.wpi.first.wpilibj.simulation.FlywheelSim;
+    import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+    import edu.wpi.first.wpilibj2.command.Command;
+    import edu.wpi.first.wpilibj2.command.SubsystemBase;
+    import edu.wpi.first.wpilibj2.command.button.Trigger;
+    import frc.robot.util.OCXboxController;
 
-import edu.wpi.first.math.MathUtil;
-import edu.wpi.first.units.measure.Angle;
-import edu.wpi.first.units.measure.AngularVelocity;
-import edu.wpi.first.units.measure.Current;
-import edu.wpi.first.units.measure.Distance;
-import edu.wpi.first.units.measure.LinearVelocity;
-import edu.wpi.first.units.measure.Voltage;
-import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
-import edu.wpi.first.wpilibj2.command.Command;
-import edu.wpi.first.wpilibj2.command.SubsystemBase;
-import edu.wpi.first.wpilibj2.command.button.Trigger;
+    import static edu.wpi.first.units.Units.*;
+    import static edu.wpi.first.wpilibj2.command.Commands.sequence;
+    import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
+    import static frc.robot.subsystems.Climber.ClimberConstants.*;
+import static frc.robot.util.OCUnits.PoundSquareInches;
 
-import static edu.wpi.first.units.Units.*;
-import static edu.wpi.first.wpilibj2.command.Commands.sequence;
-import static edu.wpi.first.wpilibj2.command.Commands.waitUntil;
-import static frc.robot.subsystems.Climber.ClimberConstants.*;
-
-    
-public class Climber extends SubsystemBase {
-    public TalonFX motor = new TalonFX(kMotorID);
-
-    private Angle targetRotationDegrees = Degrees.of(30000);
-
-    private final StatusSignal<Angle> positionStatus = motor.getPosition();
-    private final StatusSignal<AngularVelocity> velocityStatus = motor.getVelocity();
-    private final StatusSignal<Voltage> voltageStatus = motor.getMotorVoltage();
-    private final StatusSignal<Current> statorStatus = motor.getStatorCurrent();
-
-    private final MotionMagicVoltage mmRequest = new MotionMagicVoltage(0).withEnableFOC(false);
-
-    public Climber(){
-        motor.getConfigurator().apply(kConfig);
-        SmartDashboard.putData("Climber/Subsystem", this);
-        resetHeight(Degrees.of(2700));
-    }
-
-    @Override
-    public void periodic(){
-        BaseStatusSignal.refreshAll(
-            positionStatus,
-            velocityStatus,
-            voltageStatus,
-            statorStatus
-        );
-        changeTunable();
-        log();
-        motor.setControl(mmRequest.withPosition(targetRotationDegrees.in(Degrees)));
-    }
-    
-    public Angle getHeight(){
-        return Degrees.of(positionStatus.getValueAsDouble());
-    }
-    
-    public Angle getTargetRotations() {
-        return targetRotationDegrees.div(360);
-    }
-
-    public LinearVelocity getLinearVelocity(){
-        return InchesPerSecond.of(velocityStatus.getValueAsDouble());// TODO: check if this works
-    }
-
-    public Voltage getVoltage(){
-        return voltageStatus.getValue();
-    }
-
-    public Current getCurrent(){
-        return statorStatus.getValue();
-    }
-
-    public void resetHeight(Angle height){
-        motor.setPosition(height.in(Degrees));
-    }
-
-    public void setVoltage(double voltage){
-        motor.setVoltage(voltage);        
-    }
-    
-    public void setHeight(Angle height){
-        height = Degrees.of(MathUtil.clamp(height.in(Degrees), minHeight.get(), maxHeight.get()));
-        targetRotationDegrees = height;
-    }
-
-    public boolean atTargetHeight(){
-        return getHeight().in(Degrees) - targetRotationDegrees.in(Degrees) <= heightTolerance.get();
-    }
-
-    public Command setVoltageC(double voltage){
-        return runOnce(()-> setVoltage(voltage)).withName("Set voltage: " + voltage);
-    }
-
-    public Command setHeightC(Angle angle){
-        return run(()-> setHeight(angle)).until(atTargetHeightT()).withName("Set height: " + angle);
-    }
-
-    public Command setMinHeightC(){
-        return setHeightC(Degrees.of(minHeight.get()));
-    }
-
-    public Command setMaxHeight(){
-        return setHeightC(Degrees.of(maxHeight.get()));
-    }
-
-    public Command climbC(){
-        return sequence(setMaxHeight(), waitUntil(atTargetHeightT()), setMinHeightC());
-    }
-
-
-    public Trigger atTargetHeightT(){
-        return new Trigger(()-> atTargetHeight()).debounce(debounceTime.get());
-    }
-
-    public void changeTunable(){
-        maxHeight.poll();
-        minHeight.poll();
-        heightTolerance.poll();
-        debounceTime.poll();
-        kP.poll();
-        kI.poll();
-        kD.poll();
-        kS.poll();
-        kV.poll(); 
-        kA.poll();
         
-        int hash = hashCode();
-     
-        if (kP.hasChanged(hash) || kI.hasChanged(hash) || kD.hasChanged(hash) || kS.hasChanged(hash) || kV.hasChanged(hash) || kA.hasChanged(hash)) {
-            kConfig.Slot0.kP = kP.get();
-            kConfig.Slot0.kI = kI.get();
-            kConfig.Slot0.kD = kD.get(); 
-            kConfig.Slot0.kS = kS.get();
-            kConfig.Slot0.kV = kV.get();
-            kConfig.Slot0.kA = kA.get(); 
-            motor.getConfigurator().apply(kConfig.Slot0);
+    public class Climber extends SubsystemBase {
+        public TalonFX motor = new TalonFX(kMotorID);
+
+        public Angle targetHeight = Degrees.of(0);
+
+        private final StatusSignal<Angle> positionStatus = motor.getPosition();
+        private final StatusSignal<AngularVelocity> velocityStatus = motor.getVelocity();
+        private final StatusSignal<Voltage> voltageStatus = motor.getMotorVoltage();
+        private final StatusSignal<Current> statorStatus = motor.getStatorCurrent();
+
+        private final PositionVoltage voltageRequest = new PositionVoltage(0);
+
+
+        public Climber(){
+            motor.getConfigurator().apply(kConfig);
+            SmartDashboard.putData("Climber/Subsystem", this);
+            resetHeight(Degrees.of(0));
+        }
+
+        @Override
+        public void periodic(){
+            BaseStatusSignal.refreshAll(
+                positionStatus,
+                velocityStatus,
+                voltageStatus,
+                statorStatus
+            );
+            changeTunable();
+            log();
+            motor.setControl(voltageRequest.withPosition(targetHeight));
+        }
+        
+        public Angle getHeight(){
+            return Degrees.of(positionStatus.getValueAsDouble());
+        }
+        
+        public Angle getTargetRotations() {
+            return targetHeight;
+        }
+
+        public AngularVelocity getVelocity(){
+            return RotationsPerSecond.of(velocityStatus.getValueAsDouble());// TODO: check if this works
+        }
+
+        public Voltage getVoltage(){
+            return voltageStatus.getValue();
+        }
+
+        public Current getCurrent(){
+            return statorStatus.getValue();
+        }
+
+        public void resetHeight(Angle height){
+            motor.setPosition(height.in(Degrees));
+        }
+
+        public void setVoltage(double voltage){
+            motor.setVoltage(voltage);        
+        }
+        
+        public void setHeight(Angle degrees){
+            degrees = Degrees.of(MathUtil.clamp(degrees.in(Degrees), minHeight.get(), maxHeight.get())); //TODO: fix climber safety before testing
+            targetHeight = degrees;
+        }
+
+        public boolean atTargetHeight(){
+            return targetHeight.in(Degrees) - getHeight().in(Degrees) <= heightTolerance.get();
+        }
+
+        public Command setVoltageC(double voltage){
+            return run(()-> setVoltage(voltage)).withName("Set voltage: " + voltage);
+        }
+
+        public Command setHeightC(Angle angle){
+            return run(()-> setHeight(angle)).withName("Set height: " + angle);
+        }
+
+        public Command setMinHeightC(){
+            return setHeightC(Degrees.of(minHeight.get()));
+        }
+
+        public Command setMaxHeightC(){
+            return setHeightC(Degrees.of(maxHeight.get()));
+        }
+
+        public Trigger atTargetHeightT(){
+            return new Trigger(()-> atTargetHeight()).debounce(debounceTime.get());
+        }
+
+        public void changeTunable(){
+            maxHeight.poll();
+            minHeight.poll();
+            heightTolerance.poll();
+            debounceTime.poll();
+            kP.poll();
+            kI.poll();
+            kD.poll();
+            kS.poll();
+            kV.poll(); 
+            kA.poll();
+            
+            int hash = hashCode();
+        
+            if (kP.hasChanged(hash) || kI.hasChanged(hash) || kD.hasChanged(hash) || kS.hasChanged(hash) || kV.hasChanged(hash) || kA.hasChanged(hash)) {
+                kConfig.Slot0.kP = kP.get();
+                kConfig.Slot0.kI = kI.get();
+                kConfig.Slot0.kD = kD.get(); 
+                kConfig.Slot0.kS = kS.get();
+                kConfig.Slot0.kV = kV.get();
+                kConfig.Slot0.kA = kA.get(); 
+                motor.getConfigurator().apply(kConfig.Slot0);
+            }   
+        }
+
+
+        public void log(){  
+            SmartDashboard.putNumber("Climber/Current Height", getHeight().in(Degrees));
+            SmartDashboard.putNumber("Climber/Target Height", getTargetRotations().in(Degrees));
+            SmartDashboard.putNumber("Climber/RPS", getVelocity().in(RotationsPerSecond));
+            SmartDashboard.putNumber("Climber/Voltage", getVoltage().in(Volts));
+            SmartDashboard.putNumber("Climber/Current", getCurrent().in(Amps));
+            SmartDashboard.putBoolean("Climber/At Height", atTargetHeightT().getAsBoolean());
+            SmartDashboard.putNumber("Climber/Height Tolerance", heightTolerance.get());
+        }
+
+        // Simulation
+        ElevatorSim climberSim = new ElevatorSim(
+            LinearSystemId.createElevatorSystem(
+                DCMotor.getKrakenX60(2),
+                climberWeight,
+                wheelRad,
+                kGearRatio
+            ),
+            DCMotor.getKrakenX60(1),
+            minHeight.get(),
+            maxHeight.get(),
+            true,
+            minHeight.get()
+        );
+
+        @Override
+        public void simulationPeriodic() {
+            var leftSim = motor.getSimState();
+            leftSim.Orientation = ChassisReference.CounterClockwise_Positive;
+            leftSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+            var rightSim = motor.getSimState();
+            rightSim.Orientation = ChassisReference.Clockwise_Positive;
+            rightSim.setSupplyVoltage(RobotController.getBatteryVoltage());
+
+            climberSim.setInput(leftSim.getMotorVoltage());
+            climberSim.update(0.02);
+
+            leftSim.setRawRotorPosition(carriageDistToMotorAngle(Meters.of(climberSim.getPositionMeters())));
+            leftSim.setRotorVelocity(carriageDistToMotorAngle(Meters.of(climberSim.getVelocityMetersPerSecond())).per(Second));
+            rightSim.setRawRotorPosition(carriageDistToMotorAngle(Meters.of(climberSim.getPositionMeters())));
+            rightSim.setRotorVelocity(carriageDistToMotorAngle(Meters.of(climberSim.getVelocityMetersPerSecond())).per(Second));
         }   
     }
-
-
-    public void log(){  
-        SmartDashboard.putNumber("Climber/Height Rotation", getHeight().in(Degrees));
-        SmartDashboard.putNumber("Climber/Target Height Rotation", targetRotationDegrees.in(Degrees));
-        SmartDashboard.putNumber("Climber/Rotatationd Per Second", getLinearVelocity().in(InchesPerSecond));
-        SmartDashboard.putNumber("Climber/Voltage", getVoltage().in(Volts));
-        SmartDashboard.putNumber("Climber/Current", getCurrent().in(Amps));
-        SmartDashboard.putBoolean("Climber/At Height?", atTargetHeightT().getAsBoolean());
-        SmartDashboard.putNumber("Climber/Height Tolerance Rotation", heightTolerance.get());
-    }
-}
-/*
- * Possibilities:
- *    Stop
- *    Is manual (variable)
- *    Is stalled
- *    Tunable numbers: DONE
-*/
+    /*
+    * Possibilities:
+    *    Stop
+    *    Is manual (variable)
+    *    Is stalled
+    *    Tunable numbers: DONE
+    */
--- src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
+++ src/main/java/frc/robot/subsystems/Climber/ClimberConstants.java
@@ -2,6 +2,9 @@ package frc.robot.subsystems.Climber;
 
 import static edu.wpi.first.units.Units.Degrees;
 import static edu.wpi.first.units.Units.Inches;
+import static edu.wpi.first.units.Units.Meters;
+import static edu.wpi.first.units.Units.Rotations;
+import static frc.robot.util.OCUnits.PoundSquareInches;
 
 import frc.robot.util.TunableNumber;
 
@@ -15,23 +18,32 @@ import com.ctre.phoenix6.signals.NeutralModeValue;
 
 import edu.wpi.first.units.measure.Angle;
 import edu.wpi.first.units.measure.Distance;
+import edu.wpi.first.units.measure.MomentOfInertia;
 
 public class ClimberConstants {
     public static final int kMotorID = 51; 
 
-    public static final Angle kMaxHeightRot = Degrees.of(3000); //TODO: tune
-    public static final Angle kMinHeightRot = Degrees.of(2300); //TODO: tune
-    public static final Angle kHeightTolerance = Degrees.of(100); //TODO: tune
+    public static final Distance kMaxHeightRot = Meters.of(2); //TODO: tune
+    public static final Distance kMinHeightRot = Meters.of(2); //TODO: tune
+    public static final Distance kHeightTolerance = Meters.of(0.2); //TODO: tune
 
-    public static final TunableNumber maxHeight = new TunableNumber("Climber/Max Height", kMaxHeightRot.in(Degrees));
-    public static final TunableNumber minHeight = new TunableNumber("Climber/Max Height", kMinHeightRot.in(Degrees));
-    public static final TunableNumber heightTolerance = new TunableNumber("Climber/Height Tolerance", kHeightTolerance.in(Degrees));
+    //CLIMBER sim
+    public static final double climberWeight = 20; //TODO: find weight
+    public static final double wheelRad = 5;
+    public static final Distance kSprocketPD = Inches.of(1.76); //TODO: find out what a spricket is
+
+
+    public static final MomentOfInertia kMomentOfInertia = PoundSquareInches.of(80);//TODO: tune
+
+    public static final TunableNumber maxHeight = new TunableNumber("Climber/Max Height", kMaxHeightRot.in(Meters));
+    public static final TunableNumber minHeight = new TunableNumber("Climber/Max Height", kMinHeightRot.in(Meters));
+    public static final TunableNumber heightTolerance = new TunableNumber("Climber/Height Tolerance", kHeightTolerance.in(Meters));
 
     public static final double kDebounceTime = 0.25;
 
     public static final TunableNumber debounceTime = new TunableNumber("Climber/Debounce Time", kDebounceTime);
 
-    public static final int kGearRatio = 1; //TODO: tune
+    public static final double kGearRatio = 1; //TODO: tune
 
     public static final TalonFXConfiguration kConfig = new TalonFXConfiguration();
     static {
@@ -39,7 +51,7 @@ public class ClimberConstants {
         feedback.SensorToMechanismRatio = kGearRatio;
         
         MotorOutputConfigs output = kConfig.MotorOutput;
-        output.NeutralMode = NeutralModeValue.Coast;
+        output.NeutralMode = NeutralModeValue.Brake;
         output.Inverted = InvertedValue.CounterClockwise_Positive; // TODO: find direction later
 
         CurrentLimitsConfigs current = kConfig.CurrentLimits;
@@ -56,13 +68,17 @@ public class ClimberConstants {
         control.kA = 0;
     }
 
-    public static final TunableNumber kP = new TunableNumber("Intake/Climber/PID/P", kConfig.Slot0.kP);
-    public static final TunableNumber kI = new TunableNumber("Intake/Climber/PID/I", kConfig.Slot0.kI);
-    public static final TunableNumber kD = new TunableNumber("Intake/Climber/PID/D", kConfig.Slot0.kD);
+    public static Angle carriageDistToMotorAngle(Distance dist) {
+        return Rotations.of(dist.in(Meters) / 2.0 / (kSprocketPD.in(Meters) * Math.PI) * kGearRatio);
+    }
+
+    public static final TunableNumber kP = new TunableNumber("Climber/PID/P", kConfig.Slot0.kP);
+    public static final TunableNumber kI = new TunableNumber("Climber/PID/I", kConfig.Slot0.kI);
+    public static final TunableNumber kD = new TunableNumber("Climber/PID/D", kConfig.Slot0.kD);
     
-    public static final TunableNumber kS = new TunableNumber("Intake/Climber/Feed Forward/S", kConfig.Slot0.kS);
-    public static final TunableNumber kV = new TunableNumber("Intake/Climber/Feed Forward/V", kConfig.Slot0.kV);
-    public static final TunableNumber kA = new TunableNumber("Intake/Climber/Feed Forward/A", kConfig.Slot0.kA);
+    public static final TunableNumber kS = new TunableNumber("Climber/Feed Forward/S", kConfig.Slot0.kS);
+    public static final TunableNumber kV = new TunableNumber("Climber/Feed Forward/V", kConfig.Slot0.kV);
+    public static final TunableNumber kA = new TunableNumber("Climber/Feed Forward/A", kConfig.Slot0.kA);
 
 
 }
