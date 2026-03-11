1e35f67f045a181b0cb6465fcb898d24698083b9
4512 Otter Chaos <otterchaos4512@gmail.com>
2026-03-10 17:52:14 -0700

    Neutral zone shooting

--- src/main/java/frc/robot/util/FieldUtil.java
+++ src/main/java/frc/robot/util/FieldUtil.java
@@ -2,6 +2,10 @@ package frc.robot.util;
 
 import static edu.wpi.first.units.Units.*;
 
+import java.util.ArrayList;
+
+import edu.wpi.first.math.geometry.Pose2d;
+import edu.wpi.first.math.geometry.Rotation2d;
 import edu.wpi.first.math.geometry.Translation2d;
 import edu.wpi.first.units.measure.Distance;
 
@@ -13,6 +17,23 @@ public class FieldUtil {
         Inches.of(182.1).in(Meters),
         kFieldWidth.div(2).in(Meters)
     );
+
+    public static final Translation2d kLeftNeutralSetpoint = new Translation2d( // TODO:tune netral zone shooting setpoints
+        kFieldLength.div(5).in(Meters),
+        kFieldWidth.div(5).in(Meters)
+    );
+
+    public static final Translation2d kRightNeutralSetpoint = new Translation2d( // TODO:tune netral zone shooting setpoints
+        kFieldLength.div(5).in(Meters),
+        kFieldWidth.minus(kFieldWidth.div(5)).in(Meters)
+    );
+
+    public static final ArrayList<Pose2d> kSetpoints;
+    static{
+        kSetpoints = new ArrayList<Pose2d>();
+        kSetpoints.add(new Pose2d(kLeftNeutralSetpoint, Rotation2d.kZero));
+        kSetpoints.add( new Pose2d(kRightNeutralSetpoint, Rotation2d.kZero));
+    }
 
     public static final Distance kHubHeight = Inches.of(72);
     public static final Distance kHubWidth = Inches.of(47);
