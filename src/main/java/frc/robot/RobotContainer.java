558438355e45394ba45cca74adaa5393618a85fd
4512 Otter Chaos <otterchaos4512@gmail.com>
2026-05-07 17:26:25 -0700

    Adding more keybinds
    for outreach stuff and also turned off some test stuff and tried to turn of fbake mode bc loud

--- src/main/java/frc/robot/RobotContainer.java
+++ src/main/java/frc/robot/RobotContainer.java
@@ -20,9 +20,11 @@ import edu.wpi.first.math.util.Units;
 import edu.wpi.first.wpilibj.DataLogManager;
 import edu.wpi.first.wpilibj.DriverStation;
 import edu.wpi.first.wpilibj.DriverStation.Alliance;
+import edu.wpi.first.wpilibj.GenericHID.RumbleType;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
 import edu.wpi.first.wpilibj2.command.Command;
+import edu.wpi.first.wpilibj2.command.Commands;
 import edu.wpi.first.wpilibj2.command.button.RobotModeTriggers;
 import edu.wpi.first.wpilibj2.command.button.Trigger;
 import frc.robot.Auto.AutoOptions;
@@ -116,15 +118,38 @@ public class RobotContainer {
             () -> drivetrain.limitTargetSpeeds(driverSpeedsSupplier.get(), drivetrain.kStandardLimiter),
             true // rotation lock
         ));
-        driver.back().onTrue(runOnce(() -> drivetrain.resetRotation(Rotation2d.kZero)));
-        driver.b().whileTrue(drivetrain.brakeC());
+        driver.back().onTrue((runOnce(() -> drivetrain.resetRotation(Rotation2d.kZero))));
+        // driver.b().whileTrue(drivetrain.brakeC());
 
         // #### Shooter Bindings
         // superstructure.doneShootingT.whileTrue(superstructure.otterShootEndControllerC(driver));
         // driver.rightTrigger().whileTrue(superstructure.otterShootOnTheSwimC(() -> drivetrain.limitTargetSpeeds(driverSpeedsSupplier.get(), drivetrain.kSOTMLimiter)));
-        driver.leftBumper().whileTrue(superstructure.otterShootStationaryC(() -> drivetrain.limitTargetSpeeds(driverSpeedsSupplier.get(), drivetrain.kSOTMLimiter)));
+        // driver.leftBumper().whileTrue(superstructure.otterShootStationaryC(() -> drivetrain.limitTargetSpeeds(driverSpeedsSupplier.get(), drivetrain.kSOTMLimiter)));
+        
+        
         driver.rightBumper().whileTrue(parallel(
             run(()-> shooter.setState(Degrees.of(23), RPM.of(2500))), 
+            sequence(
+                waitSeconds(0.3),
+                superstructure.autoIndexNoConstraints()
+            )
+        ));
+        driver.rightTrigger().whileTrue(parallel(
+            run(()-> shooter.setState(Degrees.of(26), RPM.of(1900))), 
+            sequence(
+                waitSeconds(0.3),
+                superstructure.autoIndexNoConstraints()
+            )
+        ));
+        driver.leftBumper().whileTrue(parallel(
+            run(()-> shooter.setState(Degrees.of(21), RPM.of(2800))), 
+            sequence(
+                waitSeconds(0.3),
+                superstructure.autoIndexNoConstraints()
+            )
+        ));
+        driver.povUp().whileTrue(parallel(
+            run(()-> shooter.setState(Degrees.of(21), RPM.of(3000))), 
             sequence(
                 waitSeconds(0.3),
                 superstructure.autoIndexNoConstraints()
@@ -148,13 +173,13 @@ public class RobotContainer {
 
         
         // #### Test Bindings
-        driver.povUp().whileTrue(parallel(
-            run(()->shooter.setState(Degrees.of(hoodAngle.get()), RPM.of(flywheelVelocity.get()))),
-            sequence(
-                waitSeconds(0.6),
-                superstructure.indexC()
-            )
-        ));
+        // driver.povUp().whileTrue(parallel(
+        //     run(()->shooter.setState(Degrees.of(hoodAngle.get()), RPM.of(flywheelVelocity.get()))),
+        //     sequence(
+        //         waitSeconds(0.6),
+        //         superstructure.indexC()
+        //     )
+        // ));
     }
 
     public Command getAutonomousCommand() {
